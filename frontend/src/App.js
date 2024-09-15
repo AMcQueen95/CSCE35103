6a6996596f972d9a282bf1f2357619168d8fa3d9
AidanM95 <amcqueenlol@gmail.com>
2024-09-15 16:48:34 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+
+    fetch(`${API_BASE_URL}/api/test`)
+      .then(response => response.text())
+      .then(data => {
+        setMessage(data);
+      })
+      .catch(error => {
+        console.error('Error fetching the message:', error);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React with Spring Boot Backend</h1>
+      <p>{message}</p>
     </div>
   );
 }
