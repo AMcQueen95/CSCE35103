13ef8d977f3c8f8ff0a6d0f385d758439a28b58d
Cason Parkinson <casonrpark@gmail.com>
2024-11-03 14:56:04 -0600

    working on backend build file

--- backend.sh
+++ backend.sh
@@ -1,5 +1,12 @@
 # Step 2: Maven is installed within the project
 
+export JAVA_HOME=/usr/lib/jvm/jdk-21
+export PATH=$JAVA_HOME/bin:$PATH
+
+# Check if JAVA_HOME is correctly set
+echo "JAVA_HOME is set to $JAVA_HOME"
+java -version
+
 # Step 3: Navigate to the backend directory
 cd backend/apache-maven-3.9.9/bin
 chmod +x mvn
