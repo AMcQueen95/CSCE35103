21666cb177bfb26c9b45bccaab87ac48c443f4e6
Eli Bosch <eli.bosch04@gmail.com>
2024-11-14 17:25:08 -0600

    Ugh

--- backend/src/main/java/com/example/backend/udp/UDPService.java
+++ backend/src/main/java/com/example/backend/udp/UDPService.java
@@ -15,15 +15,16 @@ public class UDPService {
     private int bufferSize = 1024;
     private int recievePort = 7501;
     private int sendPort = 7500;
-    private String host = "localhost";
+    private InetAddress address;
 
     public UDPService() {
     }
 
     public void start() {
         try {
-            this.receiveSocket = new DatagramSocket(this.recievePort); // Listening on port 7501
-            this.sendSocket = new DatagramSocket(this.sendPort);
+            this.address = InetAddress.getByName("localhost");
+            this.receiveSocket = new DatagramSocket(); // Listening on port 7501
+            this.sendSocket = new DatagramSocket();
             // Start receiving packets in a new thread
             new Thread(this::receiveDatagram).start();
         } catch (Exception e) {
@@ -45,12 +46,11 @@ public class UDPService {
         
         try {
             byte[] sendBuffer = code.getBytes(StandardCharsets.UTF_8); //Turns the code into string
-            InetAddress address = InetAddress.getByName(this.host); //converts localhost to address
 
-            DatagramPacket packet = new DatagramPacket(sendBuffer, sendBuffer.length, address, this.sendPort); //Creates packet w/information
+            DatagramPacket packet = new DatagramPacket(sendBuffer, sendBuffer.length, this.address, this.sendPort); //Creates packet w/information
             this.sendSocket.send(packet); // Send the datagram
 
-            System.out.println("UDP packet sent with Code: " + code + " to " + this.host + " : " + this.sendPort);
+            System.out.println("UDP packet sent with Code: " + code + " over \"localhost\" and port: " + this.sendPort);
 
         } catch (Exception e) {
             e.printStackTrace(); // Handle exceptions appropriately
@@ -66,9 +66,9 @@ public class UDPService {
 
         try {
             byte[] receiveBuffer = new byte[this.bufferSize]; // Buffer to hold incoming datagrams
-            
+
             while (true) {
-                DatagramPacket packet = new DatagramPacket(receiveBuffer, this.bufferSize);
+                DatagramPacket packet = new DatagramPacket(receiveBuffer, this.bufferSize, this.address, this.recievePort);
                 this.receiveSocket.receive(packet); // Receive the packet
 
                 String received = new String(packet.getData(), "UTF-8");
@@ -77,7 +77,7 @@ public class UDPService {
                  *  WebController.functionName(recieved)
                  * 
                  */
-                System.out.println("Received String: " + received); // Handle the received ID
+                System.out.println("Received String: \"" + received + "\""); // Handle the received ID
             }
         } catch (Exception e) {
             e.printStackTrace();
